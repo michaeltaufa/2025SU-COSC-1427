c7ded6463d56a047df3de7f0fb3559a8100407c4
michaeltaufa <michael.taufa@gmail.com>
2025-07-14 17:43:09 -0500

    Created programs to practice output and GUI outputs.

---
+++ Week1/FirstDialog.java
@@ -0,0 +1,19 @@
+/*
+Program Name: First Dialog
+Description:
+  This program focuses on utilizgin GUI to display string output.
+  We are importing the class 'JOptionPane' from javax.swing, where
+  we can use GUI elements.
+Created By: Michael Hikuleotmaufa
+Date: 2025-07-14
+*/
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog{
+  public static void main(String[] args){
+
+    // 'JOptionPane' is from the import statement at line 5
+    JOptionPane.showMessageDialog(null, "First Java Dialog\nThis is fun.");
+  }
+}
---
+++ Week1/Hello.java
@@ -0,0 +1,15 @@
+/*
+Program Name: Hello
+Description:
+  This program is designed to display string output and practice creating comments in Java.
+
+Created By: Michael Hikuleotmaufa
+Date: 2025-07-14
+*/
+
+public class Hello {
+  public static void main (String[] args){
+    System.out.println("Hello World from Java!");
+    System.out.println("I am learning how to program in Java.");
+  }
+}
