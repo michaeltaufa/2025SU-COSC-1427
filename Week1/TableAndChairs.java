1fca712df69804e2654ee17af230f881a26c6dd7
michaeltaufa <michael.taufa@gmail.com>
2025-07-15 18:25:40 -0500

    Completed Exercise 1-8

---
+++ Week1/TableAndChairs.java
@@ -0,0 +1,24 @@
+/*
+Program Name: Table And Chairs 
+Assignment:
+  Write, compile, and test a class that displays the 
+  pattern shown in Figure 1-26. Save the class as TableAndChairs.java.
+
+Program Description:
+  This program will provide an output image of 2 chairs and a table using
+  'X' and whitespaces.
+
+Created By: Michael Hikuleotmaufa
+Date: 2025-07-15
+*/
+
+public class TableAndChairs{
+  public static void main(String[] args) {
+   System.out.println("X                      X"); 
+   System.out.println("X                      X"); 
+   System.out.println("X      XXXXXXXXXX      X"); 
+   System.out.println("XXXXX  X        X  XXXXX"); 
+   System.out.println("X   X  X        X  X   X"); 
+   System.out.println("X   X  X        X  X   X"); 
+  }
+}
