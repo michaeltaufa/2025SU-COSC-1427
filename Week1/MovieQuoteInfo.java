f9dad3317ad43ff460944f96a973e6e92395e380
michaeltaufa <michael.taufa@gmail.com>
2025-07-14 19:33:12 -0500

    Completed Exercise 1-7

---
+++ Week1/MovieQuoteInfo.java
@@ -0,0 +1,26 @@
+/*
+Program Name: Movie Quote Info 
+Assignment:
+  Write, compile, and test a class that uses four println() statements to 
+  display, in order, your favorite movie quote, the movie it comes from, the 
+  character who said it, and the year of the movie. Save the class as 
+  MovieQuoteInfo.java.
+
+Program Description:
+
+Created By: Michael Hikuleotmaufa
+Date: 2025-07-14
+*/
+
+public class MovieQuoteInfo {
+  public static void main(String[] args){
+    System.out.println("My first movie quote is:\n'You could not live with your own failure. Where did that bring you? Back to me.' - Thanos\nMovie: Avengers End Game (2019)\n");
+
+    System.out.println("My second movie quote is:\n'If you're good at something, never do it for free.' - Joker \nMovie: The Dark Knight (2008)\n");
+
+    System.out.println("My third movie quote is:\n'I spent my life trying not to be careless. Women and children can be carless, but not men.' - Vito Corleone\nMovie: The Godfather (1972)\n");
+
+    System.out.println("My last movie quote is:\n'Me, I am dishonest. And a dishonest man you can always trust to be dishonest. Honestly, it is the honest ones you want to watch out for.' - Captain Jack Sparrow \nMovie: Pirates Of The Caribbean: The Curse Of The Black Pearl (2003) \n");
+
+  }
+}
