85d3a255056f3d1a58184f5de38627887f0f9a6e
michaeltaufa <michael.taufa@gmail.com>
2025-07-15 18:43:55 -0500

    Completed Exercise 1-9

---
+++ Week1/Triangle.java
@@ -0,0 +1,25 @@
+/*
+Program Name: Triange 
+Assignment:
+  Write, compile, and test a class that displays the 
+  pattern shown in Figure 1-27. Save the class as Triangle.java.
+
+Program Description:
+  This program will produce an image of a triangle/ pyramid
+  constructed in a string form of 'T' and whitespaces.
+
+Created By: Michael Hikuleotmaufa
+Date: 2025-07-15
+*/
+
+public class Triangle {
+  public static void main(String[] args) {
+    System.out.println("      T      ");
+    System.out.println("     TTT     "); 
+    System.out.println("    TTTTT    "); 
+    System.out.println("   TTTTTTT   "); 
+    System.out.println("  TTTTTTTTT  "); 
+    System.out.println(" TTTTTTTTTTT "); 
+    System.out.println("TTTTTTTTTTTTT"); 
+  }
+}
