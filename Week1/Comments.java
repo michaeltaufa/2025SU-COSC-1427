22cbabee391719da462d1a5fbd3df3312b9611da
michaeltaufa <michael.taufa@gmail.com>
2025-07-15 18:53:13 -0500

    Completed Exercise 1-10

---
+++ Week1/Comments.java
@@ -0,0 +1,38 @@
+/*
+Program Name: Comments 
+Assignment:
+  Write, compile, and test a class that displays the 
+  following statement about comments on two lines:
+    'Program comments are nonexecuting statements
+    you add to a file for documentation.'
+
+Program Description:
+  This program is design to practice and use the 3 different
+  styles of comments in Java.
+
+Created By: Michael Hikuleotmaufa
+Date: 2025-07-15
+*/
+
+public class Comments{
+  public static void main(String[] args) {
+    // Program comments are nonexecuting statements you add to file for a documentation.
+
+    System.out.println("Open the source code to read the comments! :)"); 
+
+    /*
+     Program comments
+     are nonexecuting
+     statements you add
+     to fill
+     for documentation.
+    */
+
+    /*
+     * Program comments are nonexecuting
+     * statements you add to fill
+     *  for documentation
+     */
+    
+  }
+}
