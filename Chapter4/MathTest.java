68a4ce17c913c252d0e106d3c29ce3fbcf4c3a9f
michaeltaufa <michael.taufa@gmail.com>
2025-09-02 18:29:02 -0500

    Practice using Math classes.

--- Chapter4/MathTest.java
+++ Chapter4/MathTest.java
@@ -29,6 +29,7 @@ public class MathTest {
   public static void main(String[] args) {
 
     int userInt;
+    double userRandomInt;
     double userDouble;
 
     Scanner userInput = new Scanner(System.in);
@@ -36,18 +37,29 @@ public class MathTest {
     System.out.println("Welcome to the 'Math Test Program'.\nFirst, enter an integer: ");
     userInt = userInput.nextInt();
 
+    System.out.println("The square root of " + userInt + " is " + Math.sqrt(userInt));
 
-
-
-
-
+    userRandomInt = (Math.random() * userInt);
+    System.out.println("The random number between 0 and " + userInt + " is " + Math.round(userRandomInt));
+    
     System.out.println("Next, enter a decimal: ");
     userDouble = userInput.nextDouble();
+    System.out.println(userDouble + "'s ceiling is " + Math.ceil(userDouble));
+    System.out.println(userDouble + "'s floor is " + Math.floor(userDouble));
+    System.out.println(userDouble + " is rounded to " + Math.round(userDouble));
 
-
-
-
+    if (userInt > userDouble){
+      System.out.println(userInt + " is the larger number and " + userDouble + " is the smaller number.");
+    }
+    else if (userDouble > userInt){
+      System.out.println(userDouble + " is the larger number and " + userInt + " is the smaller number.");
+    }
+    else {
+      System.out.println("It appears that the int " + userInt + " and the double " + userDouble + " is equal to each other.");
+    }
+    
 
     userInput.close();
+    System.out.println("Program is closed.");
   }
 }
