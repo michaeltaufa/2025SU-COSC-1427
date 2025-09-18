c422387caa4968f06cf70afb787f157da4f6e1a3
michaeltaufa <michael.taufa@gmail.com>
2025-09-17 20:57:55 -0500

    Practice using LocalDate class.

---
+++ Chapter4/DaysTilNextMonth.java
@@ -0,0 +1,51 @@
+/*
+Program Name: Days Til Next Month Program 
+
+Assignment:
+The LocalDate class includes an instance method named 
+lengthOfMonth() that returns the number of days in the month. 
+
+Write an application that allows the user to enter a month, day, and year 
+and uses methods in the LocalDate class to calculate how many days are 
+left until the first day of next month. 
+
+For example, if the user enters 6 and 28 for the month and the day, the 
+output should be There are 3 days until JULY starts. 
+
+Save the file as DaysTilNextMonth.java.
+
+Created By: Michael Hikuleotmaufa
+Date: 2025-09-17
+*/
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class DaysTilNextMonth {
+  public static void main(String[] args) {
+
+    int userMonth;
+    int userDay;
+    int userYear;
+
+    Scanner userInput = new Scanner(System.in);
+
+    System.out.println("Please enter the month: ");
+    userMonth = userInput.nextInt();
+
+    System.out.println("Please enter the day: ");
+    userDay = userInput.nextInt();
+
+    System.out.println("Please enter the year: ");
+    userYear = userInput.nextInt();
+
+    LocalDate userDate = LocalDate.of(userYear, userMonth, userDay);
+    System.out.println("Result: " + userDate);
+
+
+
+
+    userInput.close();
+    System.out.println("Program closed.");
+  }
+}
--- Chapter4/TestMonthHandling.java
+++ Chapter4/TestMonthHandling.java
@@ -2,7 +2,8 @@
 Program Name: Test Month Handling 
 
 Assignment:
-Write a program that declares two LocalDate objects and prompts the user for values. 
+Write a program that declares two LocalDate objects and prompts the user for values.
+
 Display output that demonstrates the dates displayed when one, two, and three months 
 are added to each of the objects. 
 
@@ -22,9 +23,45 @@ public class TestMonthHandling {
 
   public static void main(String[] args) {
 
+    int userMonth;
+    int userDate;
+    int userYear;
+
+    int userMonth1;
+    int userDate1;
+    int userYear1;
+
+
     Scanner userInput = new Scanner(System.in);
 
-    System.out.println(LocalDate.now());
+    System.out.println("Please enter the first month: ");
+    userMonth = userInput.nextInt();
+
+    System.out.println("Please enter the first date: ");
+    userDate = userInput.nextInt();
+
+    System.out.println("Please enter the first year: ");
+    userYear = userInput.nextInt();
+
+    LocalDate userDateFinal1 = LocalDate.of(userYear, userMonth, userDate);
+    System.out.println("First Date: " + userDateFinal1);
+
+
+
+
+    System.out.println("Please enter the second month: ");
+    userMonth1 = userInput.nextInt();
+
+    System.out.println("Please enter the second date: ");
+    userDate1 = userInput.nextInt();
+
+    System.out.println("Please enter the second year: ");
+    userYear1 = userInput.nextInt();
+
+    LocalDate userDateFinal2 = LocalDate.of(userYear1, userMonth1, userDate1);
+    System.out.println("Second Date: " + userDateFinal2);
+    
+
 
 
 
